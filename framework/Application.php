5ebfd9b7058cc72657c17a308526f4960673d567
Denis-test <1ned@rambler.ru>
2016-02-13 20:14:10 -0500

    Small correction in Application

--- framework/Application.php
+++ framework/Application.php
@@ -9,9 +9,9 @@ class Application {
 		$router = new Router(include('../app/config/routes.php'));
 		$route =  $router->parseRoute($_SERVER['REQUEST_URI']);
 		if(!empty($route)){
-			echo 'Route was not found';
-		} else {
 			echo 'Route was found';
+		} else {
+			echo 'Route was not found';
 		}
 	}
 } 
